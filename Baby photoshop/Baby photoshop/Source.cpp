11a18b5fb9c75420765a55bc68b368d702f06392
Abdallah Gamal <abdallahgamal987@gmail.com>
2024-03-26 22:46:10 +0200

    project file

---
+++ Baby photoshop/Baby photoshop/Source.cpp
@@ -0,0 +1,16 @@
+#include "Image_Class.h"
+using namespace std;
+
+int main() {
+	Image image1("arrow.jpg");
+	for (int i = 0; i < image1.width; i++){
+		for (int j = 0; j < image1.height; j++){
+			for (int z = 0; z < image1.channels; z++){
+
+
+			}
+
+		}
+
+	}
+}
