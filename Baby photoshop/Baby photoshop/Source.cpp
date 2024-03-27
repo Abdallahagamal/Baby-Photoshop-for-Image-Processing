3d55effc4ea001ad941db03d8037b3c87628edc0
Abdallah Gamal <abdallahgamal987@gmail.com>
2024-03-27 12:32:49 +0200

    invert image filter

--- Baby photoshop/Baby photoshop/Source.cpp
+++ Baby photoshop/Baby photoshop/Source.cpp
@@ -1,16 +1,26 @@
+#include <iostream>
 #include "Image_Class.h"
 using namespace std;
-
-int main() {
-	Image image1("arrow.jpg");
-	for (int i = 0; i < image1.width; i++){
-		for (int j = 0; j < image1.height; j++){
-			for (int z = 0; z < image1.channels; z++){
-
+void invertimage() {
+	string filename,filename2;
+	cin >> filename;
+	Image image1(filename);
+	for (int i = 0; i < image1.width; i++) {
+		for (int j = 0; j < image1.height; j++) {
+			for (int k = 0; k < image1.channels; k++) {
+				image1(i, j, k) = 255 - image1(i, j, k);
 
 			}
 
 		}
 
 	}
+	cin >> filename2;
+	image1.saveImage(filename2);
+	system(filename2.c_str());
+	return;
+}
+
+int main() {
+	
 }
